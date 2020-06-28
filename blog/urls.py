aa7d052e62aad8167741f122649ce7e569ca2059
reni-hristova <renihrs@outlook.com>
2020-06-28 21:51:35 +0300

    Added a template for a blog post

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,7 @@ from .              import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
+
+#<int:pk> – integer expected that will transfer to a view as a variable called pk (primary key)
