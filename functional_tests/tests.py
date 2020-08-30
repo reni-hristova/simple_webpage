1e17a026f8f88c388eb270b54f9dd1ab5ea9d98e
reni-hristova <renihrs@outlook.com>
2020-08-30 15:34:11 +0300

    Fixed navigation issue for 'CV'. Added functional_tests.

---
+++ functional_tests/tests.py
@@ -0,0 +1,6 @@
+from    selenium                        import webdriver
+from    selenium.common.exceptions      import WebDriverException
+from    selenium.webdriver.common.keys  import Keys
+from    django.test                     import LiveServerTestCase, TestCase
+import  unittest
+import  time
